6eeb15fc4375683406c5b01f2d8fe096cb4ecbc6
JGT <jgt@mac.com>
2021-12-05 19:23:50 +0000

    simple app delete

--- server.py
+++ server.py
@@ -248,7 +248,8 @@ def post_reboot(handler):
 class RESTRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         self.routes = {
-            r'^/$': {'file': 'web/index.html', 'media_type': 'text/html'},
+            r'^/$': {'file': 'web/idx.html', 'media_type': 'text/html'},
+            r'^/full$': {'file': 'web/index.html', 'media_type': 'text/html'},
             r'^/devices$': {'GET': get_devices, 'media_type': 'application/json'},
             r'^/screenshot': {'GET': get_screenshot, 'media_type': 'image/png', 'cache_type': 'no-cache, no-store, must-revalidate'},
             r'^/logcat': {'GET': get_logcat, 'media_type': 'text/plain'},
